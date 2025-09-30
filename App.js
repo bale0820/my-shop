9a87800b026110f9ba73d1e4c81db20adc0886a7
bale0820 <dbsgywls667@gmail.com>
2025-10-01 00:53:25 +0900

    Revert "feat: 장바구니 기능 추가 (추가/삭제)"

--- App.js
+++ App.js
@@ -1,30 +1,9 @@
-import { useState } from "react";
-import { addToCart, removeFromCart } from "./cart";
-
+// src/App.js
 function App() {
-  const [cart, setCart] = useState([]);
-
-  const handleAdd = () => {
-    const newItem = { id: Date.now(), name: "상품" };
-    setCart(addToCart(cart, newItem));
-  };
-
-  const handleRemove = (id) => {
-    setCart(removeFromCart(cart, id));
-  };
-
   return (
     <div>
       <h1>My Shop</h1>
-      <button onClick={handleAdd}>장바구니 담기</button>
-      <ul>
-        {cart.map(item => (
-          <li key={item.id}>
-            {item.name}
-            <button onClick={() => handleRemove(item.id)}>삭제</button>
-          </li>
-        ))}
-      </ul>
+      <p>환영합니다!</p>
     </div>
   );
 }
