c0d6ec0fe8cc7365ff99c07359373530984c89fb
unknown <dbsgywls667@gmail.com>
2025-10-01 00:30:55 +0900

    feat: 장바구니 기능 추가 (추가/삭제)

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
-// src/App.js
+import { useState } from "react";
+import { addToCart, removeFromCart } from "./cart";
+
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const handleAdd = () => {
+    const newItem = { id: Date.now(), name: "상품" };
+    setCart(addToCart(cart, newItem));
+  };
+
+  const handleRemove = (id) => {
+    setCart(removeFromCart(cart, id));
+  };
+
   return (
     <div>
       <h1>My Shop</h1>
-      <p>환영합니다!</p>
+      <button onClick={handleAdd}>장바구니 담기</button>
+      <ul>
+        {cart.map(item => (
+          <li key={item.id}>
+            {item.name}
+            <button onClick={() => handleRemove(item.id)}>삭제</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
